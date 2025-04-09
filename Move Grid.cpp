fd97e2ff7cbc2bd6d003600e8efb23bce6936458
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-04-09 22:09:48 +0600

    Create Move Grid.cpp

---
+++ Move Grid.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int a,b,c,d;
+cin>>a>>b>>c>>d;
+int ans=0,ans2=0;
+ans+=a;
+ans2+=b;
+ans-=c;
+ans2-=d;
+cout<<ans<<" "<<ans2<<endl;
+
+}
