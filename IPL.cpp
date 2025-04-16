cb34cad29a8266e4fdf357f315007b2ef3298084
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-04-16 20:55:34 +0600

    Create IPL.cpp

---
+++ IPL.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if (X >= 7)
+        cout << "THALA" << endl;
+    else
+        cout << "BOOM" << endl;
+
+    return 0;
+}
