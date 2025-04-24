5a95a4347a0e6e4cb8b60350f4b5f5e5410c553c
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-04-24 07:50:16 +0600

    Create Chef and Chocolates.cpp

---
+++ Chef and Chocolates.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+        int sun=x*5 + y*10;
+        int ans=sun/z;
+        cout<<ans<<endl;
+    }
+	
+
+}
