c3392a6ed8a8bb4fae3ac2ad6fb00fdb48bc18fd
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-05-24 10:27:54 +0600

    Create Capital Gain Tax.cpp

---
+++ Capital Gain Tax.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x,y;
+	cin>>x>>y;
+	if(x==y) cout<<"SAME"<<endl;
+	else if(x<y) cout<<"INCREASED"<<endl;
+	else cout<<"DECREASED"<<endl;
+
+}
