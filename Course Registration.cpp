ab7022eaba25e0dbe4110a3de05eeb5fdd1c7dbc
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-06-01 09:32:19 +0600

    Create Course Registration.cpp

---
+++ Course Registration.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+	int x,y,z;
+	cin>>x>>y>>z;
+    if((y-z)>=x) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    }
+}
