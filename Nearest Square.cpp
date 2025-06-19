423389eb8b5d368a0f85d136a86dffcd74990110
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-06-19 10:19:38 +0600

    Create Nearest Square.cpp

---
+++ Nearest Square.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        int B = (int)sqrt(N);
+        int A = B * B;
+        cout << A << endl;
+    }
+    return 0;
+}
