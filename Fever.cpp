44e51e303dc626281fce884a9a04a98be8b891ff
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-06-28 16:42:18 +0600

    Create Fever.cpp

---
+++ Fever.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--){
+    int x;
+    cin>>x;
+    if(98<x) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+}
