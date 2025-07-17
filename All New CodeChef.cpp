f78ff825364bf9f8016aad64b3c986c7f43bb3d0
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-07-17 09:41:05 +0600

    Create All New CodeChef.cpp

---
+++ All New CodeChef.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin>>a>>b;
+	if(a>b) cout<<"New"<<endl;
+	else if(a<b) cout<<"Old"<<endl;
+	else cout<<"Same"<<endl;
+
+}
